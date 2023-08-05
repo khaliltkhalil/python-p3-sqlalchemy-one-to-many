8f634720d16a517ee76217985fec719301f0df10
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-05 13:13:19 -0500

    write solutins

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
 from models import Game, Review
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///one_to_many.db')
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///one_to_many.db")
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    import ipdb; ipdb.set_trace()
+    import ipdb
+
+    ipdb.set_trace()
